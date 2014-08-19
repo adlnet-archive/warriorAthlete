3582ba2f0426652088c844ebe8d272e7edc08d99
rchadwic <rchadwic@gmail.com>
2014-08-19 16:36:24 -0400

    first commit for hook

---
+++ deployHookServer/index.js
@@ -0,0 +1,35 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express'),
+  http = require('http'),
+  child_process = require('child_process');
+
+var app = express();
+var server = app.listen(3000);
+
+
+
+app.use(express.static(__dirname + '/public'));
+//  app.use(require('body-parser'));
+//  app.use(require('method-override'));
+app.get('/servicehook', function(req, res, next) {
+  console.log('hook was hit');
+  res.status(200).end();
+
+  child_process.execFile('git', ['reset','--hard'],{cwd:__dirname + '/../'}, function(e,s,o) {
+    console.log(e);
+    console.log(s);
+    console.log(o);
+    child_process.execFile('git', 'pull', function(e,s,o) {
+      console.log(e);
+      console.log(s);
+      console.log(o);
+      process.exit();
+    })
+  })
+  next();
+});
+
+
+console.log("Express server listening on port 3000");
