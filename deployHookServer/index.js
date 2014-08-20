f28492560c4b774768cee82b7c00bd8cc0654ad2
Rob Chadwick <rchadwic@gmail.com>
2014-08-20 09:12:07 -0400

    format nicely, add async.js, respond with log

--- deployHookServer/index.js
+++ deployHookServer/index.js
@@ -2,8 +2,9 @@
  * Module dependencies.
  */
 var express = require('express'),
-  http = require('http'),
-  child_process = require('child_process');
+    http = require('http'),
+    child_process = require('child_process'),
+    async = require('async');
 
 var app = express();
 var server = app.listen(3000);
@@ -13,34 +14,52 @@ app.use(express.static(__dirname + '/public'));
 app.use(require('body-parser').json());
 //  app.use(require('method-override'));
 app.post('/servicehook', function(req, res, next) {
-  console.log('hook was hit');
-  res.status(200).end();
+    console.log('hook was hit');
 
-  
+    async.series([
 
-  child_process.execFile('git', ['reset','--hard'],{cwd:__dirname + '/../'}, function(e,s,o) {
-     console.log(e);
-    console.log(s);
-    console.log(o);
-    child_process.execFile('git', ['pull'],{cwd:__dirname + '/../'}, function(e,s,o) {
-      console.log(e);
-      console.log(s);
-      console.log(o);
+            //first, lets reset the entire git repo
+            function(cb) {
+                child_process.execFile('git', ['reset', '--hard'], {
+                    cwd: __dirname + '/../'
+                }, function(e, s, o) {
+                    console.log(e);
+                    console.log(s);
+                    console.log(o);
+                    cb();
+                })
+            },
+            //now, 
+            function(cb) {
+                child_process.execFile('git', ['pull'], {
+                    cwd: __dirname + '/../'
+                }, function(e, s, o) {
+                    console.log(e);
+                    console.log(s);
+                    console.log(o);
+                    cb();
+                })
 
-      //thsi is a test
+            },
+            function(cb) {
+                child_process.execFile('git', ['status'], {
+                    cwd: __dirname + '/../'
+                }, function(e, s, o) {
+                    console.log(e);
+                    console.log(s);
+                    console.log(o);
+                    res.status(200).write(s).end();
+                    cb();
+                })
 
-  
-
-//shadlentrixt
-
-
-
-console.log(req.body.ref);
-
-      process.exit();
-    })
-  })
-  next();
+            }
+        ],
+        function() {
+            console.log(req.body.ref);
+            process.exit();
+        });
+    //shadlentrixt
+    next();
 });
 
 
