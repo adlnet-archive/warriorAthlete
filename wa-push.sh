1fec754860e4d69cc59404be1f9c943bae50278e
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2014-10-21 08:47:02 -0400

    Fixed push/pull scripts (pending testing).

--- wa-push.sh
+++ wa-push.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-cd /edx/app/edxapp/edx-platform
-sudo  /edx/bin/python.edxapp ./manage.py cms --settings aws export ADL/WA_101/2014_T1  /warriorAthlete/course
-cd /warriorAthlete
-git add .
-git commit -m "$1"
-git pull
-git push
+cd /edx/app/edxapp/edx-platform;
+sudo /edx/bin/python.edxapp ./manage.py cms --settings aws export ADL/WA_101/2014_T1  /warriorAthlete/course;
+cd /warriorAthlete;
+git add course;
+git commit -m "$1";
+git fetch;
+if git merge origin/development;
+then
+	git push origin development;
+else
+	echo "Automatic merge failed. You have changed some of the same files as upstream. See an admin."
+fi
