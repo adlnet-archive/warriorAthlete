a122b5e98a9b7739a5888bf9df82d42830c67380
Rob Chadwick <rchadwic@gmail.com>
2014-12-01 10:48:57 -0500

    use media queries instead of JS to size text into boxes

--- NutritionGame/game.js
+++ NutritionGame/game.js
@@ -22,6 +22,7 @@ function bringFront(elem, stack) {
 
 function adjustHeights(index, elem) {
 
+    return;
     var fontstep = 2;
     if ($(elem).height() > $(elem).parent().height() || $(elem).width() > $(elem).parent().width()) {
         $(elem).css('font-size', (($(elem).css('font-size').substr(0, 2) - fontstep)) + 'px').css('line-height', (($(elem).css('font-size').substr(0, 2))) + 'px');
